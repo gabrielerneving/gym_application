b3794f784bce49eabb7dcd90baf5cc8fa1fd43ce
gaer3404 <gabriel.erneving@gmail.com>
2025-10-09 13:37:22 +0200

    nerdy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,8 +8,18 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+import java.util.Properties
+import java.io.FileInputStream
+
+// Load keystore properties
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties = Properties()
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.gym_app"
+    namespace = "com.gabrielerneving.repicore"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -24,7 +34,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.gym_app"
+        applicationId = "com.gabrielerneving.repicore"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
@@ -33,11 +43,18 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
---
+++ android/app/src/main/kotlin/com/gabrielerneving/repicore/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.gabrielerneving.repicore
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
