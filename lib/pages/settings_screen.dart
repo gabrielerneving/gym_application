ca7b616fdc43eb362307ac5bd516f0ad41927774
gaer3404 <gabriel.erneving@gmail.com>
2025-12-10 17:14:41 +0100

    ny version

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -295,7 +295,7 @@ class SettingsScreen extends ConsumerWidget {
               icon: Icons.info_outline,
               title: 'Version',
               trailing: Text(
-                '1.1.1',
+                '1.2.1',
                 style: TextStyle(
                   color: currentTheme.textSecondary,
                   fontSize: 14,
